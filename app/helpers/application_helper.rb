0d66b4063a56b2523a9a5e669a1f77abdef0d093
Gorden Wunderlich <Gorden.Wunderlich@uni-duesseldorf.de>
2017-10-26 14:22:39 +0200

    audits now show drink names

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,14 +5,12 @@ module ApplicationHelper
     sprintf('%.2f EUR', price)
   end
 
-  def link_to_drink_if_exists(drink)
+  def drink_name(drink)
     drink = Drink.find_by(:id => drink)
     if drink.nil?
       return "n/a"
     else
-      return link_to drink do
-        drink.name
-      end
+      return drink.name
     end
   end
 end
