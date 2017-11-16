0097c824820a2fb33456049f92722d31483b992c
Gorden Wunderlich <Gorden.Wunderlich@uni-duesseldorf.de>
2017-11-16 08:41:12 +0100

    smaller user icons

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,6 +3,6 @@ module UsersHelper
   # see also: /config/initializers/gravatar_image_tag.rb
   
   def gravatar(user)
-    gravatar_image_tag user.email, class: user.active? ? "" : "disabled"
+    gravatar_image_tag user.email, gravatar: {size: 40}, class: user.active? ? "" : "disabled"
   end
 end
